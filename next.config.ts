3eff30bd2860c3146521fc23672ba8791b5f6cd6
Felipe Hiba <fhiba@itba.edu.ar>
2025-03-24 13:15:51 -0300

    modified next config to disable eslint during builds

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Disables ESLint for production builds
+  },
 };
 
 export default nextConfig;
